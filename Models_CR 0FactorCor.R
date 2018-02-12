800376e404a5722daf9dc234e7af7d0a5763e63d
cathyxijuan <cathyxijuan@gmail.com>
2018-02-12 05:12:27 -0800

    adding model file

---
+++ Models_CR 0FactorCor.R
@@ -0,0 +1,119 @@
+library(lavaan)
+
+
+
+fitted.mod <- '     
+f1 =~ NA*x1 + x2 + x3 +x4 + x5 + x6
+f2 =~ NA*x7 + x8 + x9 +x10 + x11 + x12
+f1 ~~ 1*f1
+f2 ~~ 1*f2'
+
+
+
+pop.mod1 <- '     
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12
+f1 ~~ 0*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~ .51*x8
+x9 ~~ .51*x9
+x10 ~~ .51*x10
+x11 ~~ .51*x11
+x12 ~~ .51*x12
+'
+
+
+
+
+pop.mod2 <- '    
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6 
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12 
+f1 ~~ 0*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~  .51*x8
+x9 ~~  .51*x9
+x10 ~~  .51*x10
+x11 ~~  .51*x11
+x12 ~~ .51*x12
+x11 ~~ 0.2*x12
+'
+
+
+
+
+
+
+
+
+
+
+
+
+
+pop.mod <- list( pop.mod1, 
+                 pop.mod2)
+
+sigma <-list()
+for(i in 1:length(pop.mod)){
+  fit <-cfa(pop.mod[[i]], data=simulateData(pop.mod[[i]], sample.nobs=1000))
+  sigma[[i]]<- lavInspect(fit, "cov.ov")
+}
+
+sigma.hat <-list()
+
+for(i in 1:length(pop.mod)){
+  
+  fit <- cfa(model=fitted.mod, sample.nobs=300, sample.cov=sigma[[i]], mimic="EQS") 
+  sigma.hat[[i]]<- lavInspect(fit, "cov.ov")
+}
+
+
+
+# 
+ fit.indices.comp <-matrix( nrow = 0, ncol = 6)
+# 
+for(i in 1:length(pop.mod)){
+   simuData <- simulateData(pop.mod[[i]], sample.nobs=1000000,seed=111)
+   fit <- cfa(fitted.mod, data=simuData)
+    fit.indices.comp<- rbind(fit.indices.comp,lavInspect(fit, "fit")[c("fmin","rmsea","cfi","srmr","gfi", "df")])
+  }
+# round(fit.indices.comp,4)
+# 
+# fit.complete.CR <- fit.indices.comp
+# save(fit.complete.CR, file="fit.complete.CR.RData")
+# save(sigma.hat, file="sigma.hat.CR.RData")
+# save(sigma, file="sigma.CR.RData")
+
+
+
+# ind <- c(5, 6,11,12)
+# #ind <- c(1:3, 7:10)
+# mod.n <-10
+# sigma.pop<- sigma[[mod.n]]
+# sigma.hat.pop <- sigma.hat[[mod.n]]
+# comp <- log(det(sigma.hat[[mod.n]])/det(sigma[[mod.n]]))
+# part1 <- log(det(sigma.hat[[mod.n]][-ind,-ind])/det(sigma[[mod.n]][-ind,-ind]))
+# round(comp, 6)<round(part1, 6)
+# 
+# comp <- log(det(sigma.hat[[mod.n]])/det(sigma[[mod.n]]))
+# part2 <- log(det(sigma.hat[[mod.n]][ind,ind])/det(sigma[[mod.n]][ind,ind]))
+# round(comp, 6)<round(part2, 6)
+
+
+
