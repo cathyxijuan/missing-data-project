08be835f9495b4a05f3f5905c1d83ded5d8ed6d5
cathyxijuan <cathyxijuan@gmail.com>
2018-02-12 07:00:53 -0800

    adding more models

--- Models_CR_DP.R
+++ Models_CR_DP.R
@@ -366,6 +366,116 @@ x12 ~~ .51*x12
 x11 ~~ 0.5*x12
 '
 
+##place of misfit and place of missing are different; 0.6 factor correlation 
+
+
+pop.mod5.1 <- '    
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6 
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12 
+f1 ~~ 0.6*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~  .51*x8
+x9 ~~  .51*x9
+x10 ~~  .51*x10
+x11 ~~  .51*x11
+x12 ~~ .51*x12
+x11 ~~ 0.1*x12
+'
+
+pop.mod5.2 <- '    
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6 
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12 
+f1 ~~ 0.6*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~  .51*x8
+x9 ~~  .51*x9
+x10 ~~  .51*x10
+x11 ~~  .51*x11
+x12 ~~ .51*x12
+x11 ~~ 0.2*x12
+'
+
+pop.mod5.3 <- '    
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6 
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12 
+f1 ~~ 0.6*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~  .51*x8
+x9 ~~  .51*x9
+x10 ~~  .51*x10
+x11 ~~  .51*x11
+x12 ~~ .51*x12
+x11 ~~ 0.3*x12
+'
+
+pop.mod5.4 <- '    
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6 
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12 
+f1 ~~ 0.6*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~  .51*x8
+x9 ~~  .51*x9
+x10 ~~  .51*x10
+x11 ~~  .51*x11
+x12 ~~ .51*x12
+x11 ~~ 0.4*x12
+'
+
+
+
+
+pop.mod5.5 <- '    
+f1 =~ .7*x1 + .7*x2 + .7*x3 +.7*x4 + .7*x5 + .7*x6 
+f2 =~ .7*x7 + .7*x8 + .7*x9 +.7*x10 + .7*x11 + .7*x12 
+f1 ~~ 0.6*f2
+f1 ~~ 1*f1
+f2 ~~ 1*f2    
+x1 ~~ .51*x1
+x2 ~~ .51*x2
+x3 ~~ .51*x3
+x4 ~~ .51*x4
+x5 ~~ .51*x5
+x6 ~~ .51*x6
+x7 ~~ .51*x7
+x8 ~~  .51*x8
+x9 ~~  .51*x9
+x10 ~~  .51*x10
+x11 ~~  .51*x11
+x12 ~~ .51*x12
+x11 ~~ 0.5*x12
+'
 
 
 
@@ -374,7 +484,8 @@ x11 ~~ 0.5*x12
 pop.mod <- list( pop.mod1, 
                  pop.mod2.1, pop.mod2.2, pop.mod2.3, pop.mod2.4, pop.mod2.5,
                  pop.mod3.1, pop.mod3.2, pop.mod3.3, pop.mod3.4, pop.mod3.5,
-                 pop.mod4.1, pop.mod4.2, pop.mod4.3, pop.mod4.4, pop.mod4.5)
+                 pop.mod4.1, pop.mod4.2, pop.mod4.3, pop.mod4.4, pop.mod4.5,
+                 pop.mod5.1, pop.mod5.2, pop.mod5.3, pop.mod5.4, pop.mod4.5)
 
 sigma <-list()
 for(i in 1:length(pop.mod)){
@@ -382,31 +493,30 @@ for(i in 1:length(pop.mod)){
   sigma[[i]]<- lavInspect(fit, "cov.ov")
 }
 
-# sigma.hat <-list()
-# 
-# for(i in 1:length(pop.mod)){
-#   
-#   fit <- cfa(model=fitted.mod, sample.nobs=300, sample.cov=sigma[[i]], mimic="EQS") 
-#   sigma.hat[[i]]<- lavInspect(fit, "cov.ov")
-# }
+ sigma.hat <-list()
+ 
+ for(i in 1:length(pop.mod)){
+   
+ fit <- cfa(model=fitted.mod, sample.nobs=300, sample.cov=sigma[[i]], mimic="EQS") 
+   sigma.hat[[i]]<- lavInspect(fit, "cov.ov")
+ }
 # 
 # 
 # 
 # # 
-#  fit.indices.comp <-matrix( nrow = 0, ncol = 6)
-# # 
-# for(i in 1:length(pop.mod)){
-#    simuData <- simulateData(pop.mod[[i]], sample.nobs=1000000,seed=111)
-#    fit <- cfa(fitted.mod, data=simuData)
-#     fit.indices.comp<- rbind(fit.indices.comp,lavInspect(fit, "fit")[c("fmin","rmsea","cfi","srmr","gfi", "df")])
-#   }
-# round(fit.indices.comp,4)
-# # 
-# fit.complete.CR <- fit.indices.comp
+ fit.indices.comp <-matrix( nrow = 0, ncol = 6)
 
-#save(fit.complete.CR, file="fit.complete.CR_DP.RData")
-#save(sigma.hat, file="sigma.hat.CR_DP.RData")
-#save(sigma, file="sigma.CR_DP.RData")
+ for(i in 1:length(pop.mod)){
+    simuData <- simulateData(pop.mod[[i]], sample.nobs=1000000,seed=111)
+    fit <- cfa(fitted.mod, data=simuData)
+    fit.indices.comp<- rbind(fit.indices.comp,lavInspect(fit, "fit")[c("fmin","rmsea","cfi","srmr","gfi", "df")])
+  }
+round(fit.indices.comp,4)
+fit.complete.CR <- fit.indices.comp
+
+save(fit.complete.CR, file="fit.complete.CR_DP.RData")
+save(sigma.hat, file="sigma.hat.CR_DP.RData")
+save(sigma, file="sigma.CR_DP.RData")
 
 
 
