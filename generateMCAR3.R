23d5e3e9fd2314c7fe72234772f7dc025646bb40
cathyxijuan <cathyxijuan@gmail.com>
2018-04-02 17:07:23 -0700

    new results and changes

--- generateMCAR3.R
+++ generateMCAR3.R
@@ -1,12 +1,12 @@
 library(lavaan)
 library(simsem)
 
-source("Models_WM1.R")
+source("Models_CR2_3.R")
 
 
 
 
-#purpose: create missing data on x1,  x2.
+#purpose: create missing data on x11,  x12.
 #Argument:
 #model: lavaan defined population model
 #sample.nobs: numeric; sample size without missing data
@@ -21,9 +21,11 @@ MCARMinPattern <- function(model, sample.nobs=1000000,  missing.percentage){
   simuData
 }
 
-#Usage: only for this research.  Two variables has missing data; maximum number of missing patterns for two variables: (1), (2) 
+
+
+#Usage: only for this research.  Two variables has missing data; maximum number of missing patterns for two variables: x11 and x12
        # Each variable with missing data has the given percentage of missing data. 
-       # It has four missing data patterns:1) missing data on X1; 2) missing data on both x1 and x2; 3) missing data on x2; 4) no missing data pattern
+       # It has four missing data patterns:1) missing data on x11; 2) missing data on x12; 3) missing data on x11 and x12; 4) no missing data pattern
      
 #Argument:
 #model: lavaan defined population model
@@ -38,11 +40,14 @@ MCARMaxPattern <- function(model, sample.nobs=1000000,  missing.percentage=.5){
                          x9=data[,"x9"], x10=data[,"x10"], x11=data[,"x11"], x12=data[,"x12"])
   
   perc.mis.per.chunk <- missing.percentage/2 # 4 missing data patterns
-  simuData[1:(sample.nobs*perc.mis.per.chunk), 1] <-NA
-  simuData[(sample.nobs*perc.mis.per.chunk+1):(sample.nobs*perc.mis.per.chunk*2),  1:2] <-NA
-  simuData[(sample.nobs*perc.mis.per.chunk*2+1):(sample.nobs*perc.mis.per.chunk*3),  2] <-NA
+  simuData[1:(sample.nobs*perc.mis.per.chunk), 11] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk+1):(sample.nobs*perc.mis.per.chunk*2),  12] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*2+1):(sample.nobs*perc.mis.per.chunk*3), 11:12] <-NA
   simuData
 }
+
+
+
 
 
 #Usage: only for this research. Two variables have missing data; maximum number of missing patterns for two variables: (1) and (2) 
@@ -56,10 +61,13 @@ MCARMaxPattern2 <- function(model, sample.nobs=1000000,  missing.percentage=.5){
   missing.percentage <- missing.percentage
   simuData <- simulateData(model, sample.nobs=sample.nobs, seed=111)
   for(i in 0:1){
-    simuData[1:(sample.nobs*missing.percentage/(2^i)),1+i] <- NA
+    simuData[1:(sample.nobs*missing.percentage/(2^i)),11+i] <- NA
   }
   simuData
 }
+
+
+
 
 #Usage: put fit indices for a list of models into a matrix
 
@@ -115,28 +123,18 @@ sigma.hat.MCAR <- function(pop.model.list, fitted.mod, sample.nobs = 1000000,  m
 
 
 
-sigmaHat_MCAR_MinPat_20PerMiss_2VarMiss_WM1 <- sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
-
-sigmaHat_MCAR_MinPat_50PerMiss_2VarMiss_WM1 <- sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
-
-
-
-fitMCAR_MinPat_20PerMiss_2VarMiss_WM1 <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
-
-
-fitMCAR_MinPat_50PerMiss_2VarMiss_WM1 <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
+# sigmaHat_MCAR_MinPat_20PerMiss_2VarMiss_CR2_3 <- sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
+# sigmaHat_MCAR_MinPat_50PerMiss_2VarMiss_CR2_3 <- sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
+# fitMCAR_MinPat_20PerMiss_2VarMiss_CR2_3 <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
+# fitMCAR_MinPat_50PerMiss_2VarMiss_CR2_3 <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
+# round(fitMCAR_MinPat_20PerMiss_2VarMiss_CR2_3,6) 
+# round(fitMCAR_MinPat_50PerMiss_2VarMiss_CR2_3,6)
 
 
-round(fitMCAR_MinPat_20PerMiss_2VarMiss_WM1,6) 
-
-round(fitMCAR_MinPat_50PerMiss_2VarMiss_WM1,6)
-
-
-
-# save(sigmaHat_MCAR_MinPat_20PerMiss_2VarMiss_WM1, file="sigmaHat_MCAR_MinPat_20PerMiss_2VarMiss_WM1.RData")
-# save(sigmaHat_MCAR_MinPat_50PerMiss_2VarMiss_WM1, file="sigmaHat_MCAR_MinPat_50PerMiss_2VarMiss_WM1.RData")
-# save(fitMCAR_MinPat_20PerMiss_2VarMiss_WM1, file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM1.RData")
-# save(fitMCAR_MinPat_50PerMiss_2VarMiss_WM1, file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM1.RData")
+# save(sigmaHat_MCAR_MinPat_20PerMiss_2VarMiss_CR2_3, file="sigmaHat_MCAR_MinPat_20PerMiss_2VarMiss_CR2_3.RData")
+# save(sigmaHat_MCAR_MinPat_50PerMiss_2VarMiss_CR2_3, file="sigmaHat_MCAR_MinPat_50PerMiss_2VarMiss_CR2_3.RData")
+# save(fitMCAR_MinPat_20PerMiss_2VarMiss_CR2_3, file="fitMCAR_MinPat_20PerMiss_2VarMiss_CR2_3.RData")
+# save(fitMCAR_MinPat_50PerMiss_2VarMiss_CR2_3, file="fitMCAR_MinPat_50PerMiss_2VarMiss_CR2_3.RData")
 
 
 
@@ -144,6 +142,29 @@ round(fitMCAR_MinPat_50PerMiss_2VarMiss_WM1,6)
 
 
 
+sigmaHat_MCAR_MaxPat_20PerMiss_2VarMiss_CR2_3 <- 
+  sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "max")
+
+sigmaHat_MCAR_MaxPat_50PerMiss_2VarMiss_CR2_3 <- 
+  sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "max")
+
+fitMCAR_MaxPat_20PerMiss_2VarMiss_CR2_3 <- 
+  fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "max")
+
+fitMCAR_MaxPat_50PerMiss_2VarMiss_CR2_3 <- 
+  fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "max")
+
+
+round(fitMCAR_MaxPat_20PerMiss_2VarMiss_CR2_3,6) 
+
+round(fitMCAR_MaxPat_50PerMiss_2VarMiss_CR2_3,6)
+
+
+
+save(sigmaHat_MCAR_MaxPat_20PerMiss_2VarMiss_CR2_3, file="sigmaHat_MCAR_MaxPat_20PerMiss_2VarMiss_CR2_3.RData")
+save(sigmaHat_MCAR_MaxPat_50PerMiss_2VarMiss_CR2_3, file="sigmaHat_MCAR_MaxPat_50PerMiss_2VarMiss_CR2_3.RData")
+save(fitMCAR_MaxPat_20PerMiss_2VarMiss_CR2_3, file="fitMCAR_MaxPat_20PerMiss_2VarMiss_CR2_3.RData")
+save(fitMCAR_MaxPat_50PerMiss_2VarMiss_CR2_3, file="fitMCAR_MaxPat_50PerMiss_2VarMiss_CR2_3.RData")
 
 
 
@@ -156,11 +177,6 @@ round(fitMCAR_MinPat_50PerMiss_2VarMiss_WM1,6)
 
 
 
-fit.MCAR.MAX50.CL <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "max")
-fit.MCAR.MAX20.CL <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "max")
-
-#save(fit.MCAR.MIN20.CR2, file="fit.MCAR.MAX20.CR3.RData")
-#save(fit.MCAR.MIN20.CR2, file="fit.MCAR.MAX50.CR3.RData")
 
 
  
--- generateMCAR4.R
+++ generateMCAR4.R
@@ -1,7 +1,7 @@
 library(lavaan)
 library(simsem)
 
-source("Models_WM1.R")
+source("Models_CR2_3.R")
 
 
 
@@ -20,6 +20,102 @@ MCARMinPattern <- function(model, sample.nobs=1000000,  missing.percentage){
   simuData[1:(sample.nobs*missing.percentage),  c(9:12)] <-NA
   simuData
 }
+
+
+
+
+
+#Usage: only for this research.  Two variables has missing data; maximum number of missing patterns for two variables:x9, x10, x11 and x12
+# Each variable with missing data has the given percentage of missing data. 
+# It has four missing data patterns:1) missing data on x9 and x10; 
+#                                   2) missing data on x11 and x12; 
+#                                   3) missing data on x9, x10, x11 and x12; 
+#                                   4) no missing data pattern
+
+#Argument:
+#model: lavaan defined population model
+#sample.nobs: numeric; sample size without missing data
+#missing.percentage: numeric; a proportion of missing data
+#missing.percentage: vector specifying which columns are missing
+MCARMaxPattern <- function(model, sample.nobs=1000000,  missing.percentage=.5){
+  missing.percentage <- missing.percentage
+  data <- simulateData(model, sample.nobs=sample.nobs, seed=111)
+  simuData <- data.frame(x1=data[,"x1"], x2=data[,"x2"], x3=data[,"x3"], x4=data[,"x4"],
+                         x5=data[,"x5"], x6=data[,"x6"], x7=data[,"x7"], x8=data[,"x8"],
+                         x9=data[,"x9"], x10=data[,"x10"], x11=data[,"x11"], x12=data[,"x12"])
+  
+  perc.mis.per.chunk <- missing.percentage/2 # 4 missing data patterns
+  simuData[1:(sample.nobs*perc.mis.per.chunk), 9:10] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk+1):(sample.nobs*perc.mis.per.chunk*2),  11:12] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*2+1):(sample.nobs*perc.mis.per.chunk*3),  9:12] <-NA
+  simuData
+}
+
+
+
+
+
+
+#Usage: only for this research.  Four variables has missing data; max number of missing patterns for four variables: x9, x10, x11 and x12
+# Each variable with missing data has the given percentage of missing data. 
+# It has four missing data patterns:1) missing data on x9; 
+#                                   2) missing data on x10; 
+#                                   3) missing data on x11; 
+#                                   4) missing data on x12; 
+#                                   5) missing data on x9 and x10; 
+#                                   6) missing data on x9 and x11; 
+#                                   7) missing data on x9 and x12; 
+#                                   8) missing data on x10 and x11; 
+#                                   9) missing data on x10 and x12; 
+#                                   10) missing data on x11 and x12; 
+#                                   11) missing data on x9, x10 and x11; 
+#                                   12) missing data on x9, x10 and x12; 
+#                                   13) missing data on x9, x11 and x12; 
+#                                   14) missing data on x10, x11 and x12; 
+#                                   15) missing data on x9, x10, x11 and x12
+#                                   16) no missing data pattern
+
+#Argument:
+#model: lavaan defined population model
+#sample.nobs: numeric; sample size without missing data
+#missing.percentage: numeric; a proportion of missing data
+#missing.percentage: vector specifying which columns are missing
+MCARMaxPattern2 <- function(model, sample.nobs=1000000,  missing.percentage=.5){
+  missing.percentage <- missing.percentage
+  num.missing.var <- 4
+  data <- simulateData(model, sample.nobs=sample.nobs, seed=111)
+  simuData <- data.frame(x1=data[,"x1"], x2=data[,"x2"], x3=data[,"x3"], x4=data[,"x4"],
+                         x5=data[,"x5"], x6=data[,"x6"], x7=data[,"x7"], x8=data[,"x8"],
+                         x9=data[,"x9"], x10=data[,"x10"], x11=data[,"x11"], x12=data[,"x12"])
+  
+  perc.mis.per.chunk <- missing.percentage/(2^num.missing.var/2) # 16 missing data patterns
+  simuData[1:(sample.nobs*perc.mis.per.chunk),  9] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk+1):(sample.nobs*perc.mis.per.chunk*2),  10] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*2+1):(sample.nobs*perc.mis.per.chunk*3),  11] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*3+1):(sample.nobs*perc.mis.per.chunk*4),  12] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*4+1):(sample.nobs*perc.mis.per.chunk*5),  c(9,10)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*5+1):(sample.nobs*perc.mis.per.chunk*6),  c(9,11)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*6+1):(sample.nobs*perc.mis.per.chunk*7), c(9,12)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*7+1):(sample.nobs*perc.mis.per.chunk*8),  c(10,11)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*8+1):(sample.nobs*perc.mis.per.chunk*9),  c(10,12)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*9+1):(sample.nobs*perc.mis.per.chunk*10),  c(11,12)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*10+1):(sample.nobs*perc.mis.per.chunk*11),  c(9,10,11)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*11+1):(sample.nobs*perc.mis.per.chunk*12), c(9,10,12)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*12+1):(sample.nobs*perc.mis.per.chunk*13),  c(9,11,12)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*13+1):(sample.nobs*perc.mis.per.chunk*14), c(10,11,12)] <-NA
+  simuData[(sample.nobs*perc.mis.per.chunk*14+1):(sample.nobs*perc.mis.per.chunk*15), 9:12] <-NA
+  simuData
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -77,25 +173,37 @@ sigma.hat.MCAR <- function(pop.model.list, fitted.mod, sample.nobs = 1000000,  m
 
 
 
-sigmaHat_MCAR_MinPat_20PerMiss_4VarMiss_WM1 <- sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
+# sigmaHat_MCAR_MinPat_20PerMiss_4VarMiss_CR2_3 <- 
+#   sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
+# sigmaHat_MCAR_MinPat_50PerMiss_4VarMiss_CR2_3 <- 
+#   sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
+# fitMCAR_MinPat_20PerMiss_4VarMiss_CR2_3 <- 
+#   fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
+# fitMCAR_MinPat_50PerMiss_4VarMiss_CR2_3 <- 
+#   fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
+# round(fitMCAR_MinPat_20PerMiss_4VarMiss_CR2_3,6) 
+# round(fitMCAR_MinPat_50PerMiss_4VarMiss_CR2_3,6)
+# 
+# save(sigmaHat_MCAR_MinPat_20PerMiss_4VarMiss_CR2_3, file="sigmaHat_MCAR_MinPat_20PerMiss_4VarMiss_CR2_3.RData")
+# save(sigmaHat_MCAR_MinPat_50PerMiss_4VarMiss_CR2_3, file="sigmaHat_MCAR_MinPat_50PerMiss_4VarMiss_CR2_3.RData")
+# save(fitMCAR_MinPat_20PerMiss_4VarMiss_CR2_3, file="fitMCAR_MinPat_20PerMiss_4VarMiss_CR2_3.RData")
+# save(fitMCAR_MinPat_50PerMiss_4VarMiss_CR2_3, file="fitMCAR_MinPat_50PerMiss_4VarMiss_CR2_3.RData")
 
-sigmaHat_MCAR_MinPat_50PerMiss_4VarMiss_WM1 <- sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
 
 
+sigmaHat_MCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3 <- 
+  sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "max2")
+sigmaHat_MCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3 <- 
+  sigma.hat.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "max2")
+fitMCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3 <- 
+  fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "max2")
+fitMCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3 <- 
+  fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "max2")
+round(fitMCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3,6) 
+round(fitMCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3,6)
 
-fitMCAR_MinPat_20PerMiss_4VarMiss_WM1 <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.20, missing.type = "min")
-
-
-fitMCAR_MinPat_50PerMiss_4VarMiss_WM1 <- fit.ind.matrix.MCAR(pop.model.list=pop.mod, fitted.mod=fitted.mod, missing.percentage = 0.50, missing.type = "min")
-
-
-round(fitMCAR_MinPat_20PerMiss_4VarMiss_WM1,6) 
-
-round(fitMCAR_MinPat_50PerMiss_4VarMiss_WM1,6)
-
-save(sigmaHat_MCAR_MinPat_20PerMiss_4VarMiss_WM1, file="sigmaHat_MCAR_MinPat_20PerMiss_4VarMiss_WM1.RData")
-save(sigmaHat_MCAR_MinPat_50PerMiss_4VarMiss_WM1, file="sigmaHat_MCAR_MinPat_50PerMiss_4VarMiss_WM1.RData")
-save(fitMCAR_MinPat_20PerMiss_4VarMiss_WM1, file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM1.RData")
-save(fitMCAR_MinPat_50PerMiss_4VarMiss_WM1, file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM1.RData")
-
+save(sigmaHat_MCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3, file="sigmaHat_MCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3.RData")
+save(sigmaHat_MCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3, file="sigmaHat_MCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3.RData")
+save(fitMCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3, file="fitMCAR_Max2Pat_20PerMiss_4VarMiss_CR2_3.RData")
+save(fitMCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3, file="fitMCAR_Max2Pat_50PerMiss_4VarMiss_CR2_3.RData")
 
