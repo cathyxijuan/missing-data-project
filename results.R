a6529603485af9f09ac72707b86aeb2ef0a91eb4
cathyxijuan <cathyxijuan@gmail.com>
2018-09-09 03:21:06 -0700

    delete

--- results.R
+++ results.R
@@ -1,31 +1,26 @@
 # Fit for no missing short 
-load("fitNoMissingShort_CR1_1.RData")
-load("fitNoMissingShort_CR1_3.RData")
-load("fitNoMissingShort_CR2_1.RData")
-load("fitNoMissingShort_CR2_3.RData")
-load("fitNoMissingShort_WM1.RData")
+load("fitNoMissingShort_1CR_DF.RData")
+load("fitNoMissingShort_1CR_SF.RData")
+load("fitNoMissingShort_2CR_DF.RData")
+load("fitNoMissingShort_2CR_SF.RData")
+load("fitNoMissingShort_WM.RData")
 
 
 #Fit for MCAR
-load("fitMCAR_CR1_1.RData")
-load("fitMCAR_CR1_3.RData")
-load("fitMCAR_CR2_1.RData")
-load("fitMCAR_CR2_3.RData")
-load("fitMCAR_WM1.RData")
-load("fitMCAR_Short_CR1_1.RData")
-load("fitMCAR_Short_CR1_3.RData")
-load("fitMCAR_Short_CR2_1.RData")
-load("fitMCAR_Short_CR2_3.RData")
-load("fitMCAR_Short_WM1.RData")
+load("fitMCAR_Short_1CR_DF.RData")
+load("fitMCAR_Short_1CR_SF.RData")
+load("fitMCAR_Short_2CR_DF.RData")
+load("fitMCAR_Short_2CR_SF.RData")
+load("fitMCAR_Short_WM.RData")
 
 
 
 
 #Fit for MAR Linear
-load("fitMAR_Linear_Short_CR1_1.RData")
-load("fitMAR_Short_CR1_3.RData")
-load("fitMAR_Short_CR2_1.RData")
-load("fitMAR_Short_CR2_3.RData")
+load("fitMAR_Short_1CR_SF.RData")
+load("fitMAR_Short_1CR_DF.RData")
+load("fitMAR_Short_2CR_SF.RData")
+load("fitMAR_Short_2CR_DF.RData")
 load("fitMAR_maxPat_Short_WM.RData")
 load("fitMAR_minPat_Short_WM.RData")
 
